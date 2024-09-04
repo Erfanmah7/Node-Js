59aa4a03ba66b098f2f65ed322dc19a67085b7a2
Erfanmah7 <efn8mah@yahoo.com>
2024-09-05 02:01:05 +0330

    add updated & save product

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -46,10 +46,37 @@ async function create(req, res) {
   }
 }
 
+async function update(req, res) {
+  try {
+    let body = "";
+    const id = req.url.split("/")[3];
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", async () => {
+      const parseBody = { ...JSON.parse(body) };
+      const product = await productsModel.findById(id);
+      if (!product) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ message: "not found any product" }));
+        res.end();
+      } else {
+        const result = await productsModel.update(id, parseBody);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify(result));
+        res.end();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 const productsController = {
   get,
   getById,
   create,
+  update,
 };
 
 module.exports = productsController;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ http
       productsController.getById(req, res);
     else if (req.url === "/api/products" && req.method === "POST")
       productsController.create(req, res);
+    else if (req.url.match(/\/api\/products\/[0-9]+/) && req.method === "PUT")
+      productsController.update(req, res);
     else notfound(res);
   })
   .listen(3000, () => console.log("server to : http://localhost:3000"));
